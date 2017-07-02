0e9a17dab091f79b1d3eee91f758e13cf3a5975c
crisfabri12 <cristhian.ocola@ucsp.edu.pe>
2017-07-02 02:46:29 -0500

    Update View_Holder.java

--- Mychat/app/src/main/java/com/example/paulo/mychat/View_Holder.java
+++ Mychat/app/src/main/java/com/example/paulo/mychat/View_Holder.java
@@ -3,14 +3,9 @@ package com.example.paulo.mychat;
 import android.content.Intent;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.view.ContextMenu;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-/**
- * Created by paulo on 6/9/2017.
- */
 
 public class View_Holder extends RecyclerView.ViewHolder {
     CardView cv;
@@ -24,13 +19,9 @@ public class View_Holder extends RecyclerView.ViewHolder {
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //int id = Integer.parseInt(categoria.getTag().toString());
-                //Toast.makeText(itemView.getContext(), categoria.getTag().toString(), Toast.LENGTH_SHORT).show();
-                //Intent i = new Intent(MainActivity.this,MensajeActivity.class);
                 Intent i = new Intent(itemView.getContext(),MensajeActivity.class);
                 i.putExtra("nom", contacto.getText().toString());
                 i.putExtra("id",contacto.getTag().toString());
-                //Toast.makeText(itemView.getContext(), contacto.getTag().toString(), Toast.LENGTH_SHORT).show();
                 itemView.getContext().startActivity(i);
             }
         });
